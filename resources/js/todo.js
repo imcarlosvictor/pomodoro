736da09b66e69971b1b70b30fd5af39d0eb28fcb
Lucas <imcvlucas@gmail.com>
2021-12-28 16:10:48 -0500

    TODO: Todo list

--- resources/js/todo.js
+++ resources/js/todo.js
@@ -1,12 +1,13 @@
 // ###########################[TODO LIST]###############################
 function showTodoList() {
     if (document.getElementById('todo-list-sidebar').classList.toggle('active')) {
-        // Show to-do list
-        document.getElementById('main-page').classList.toggle('active');
         // Move timer and spotify to the side
+        document.getElementsById('pomodoro').classList.toggle('active');
+        // document.getElementsById('pomodoro').classList.style.gridColumnStart = 3;
+        // document.getElementsById('pomodoro').classList.style.gridColumnEnd = 5;
     } else {
         // Hide to-do list
-        document.getElementById('main-page').classList.toggle('active');
+        // document.getElementById('main-page').classList.toggle('active');
         // Reposition the timer and spotify to the center
     }
 }
